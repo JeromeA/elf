dc9ecbad312e88c2dffdafe4559956dc612fe785
JeromeA <jerome.abela@gmail.com>
2024-11-16 22:44:58 +0000

    Make section offset and addr calculations pure.

--- elf_defaults.c
+++ elf_defaults.c
@@ -15,72 +15,113 @@ static bool is_file_only_section(const ElfBinary *binary, const Elf64_Shdr *shdr
     return false;
 }
 
-static void calculate_section_offsets(const ElfBinary *binary) {
+Elf64_Off get_default_section_offset(const ElfBinary *binary, int target_segnum) {
     Elf64_Off current_offset = binary->ehdr.e_ehsize + binary->ehdr.e_phnum * binary->ehdr.e_phentsize;
-    Elf64_Addr current_addr = current_offset;
-
     int previous_flags = 0;
-    for (int segnum = 0; segnum < binary->ehdr.e_shnum; segnum++) {
+
+    for (int segnum = 0; segnum <= target_segnum && segnum < binary->ehdr.e_shnum; segnum++) {
         Elf64_Shdr *shdr = &binary->shdrs[segnum];
+
+        // Align current offset
         if (shdr->sh_addralign > 0) {
             Elf64_Off align = shdr->sh_addralign;
             current_offset = (current_offset + align - 1) & ~(align - 1);
-            current_addr = (current_addr + align - 1) & ~(align - 1);
         }
-        // Align to 4096 page if the segment changed (detected by flag change)
+
+        // Page alignment if segment flags changed
         int flags = shdr->sh_flags & SHF_EXECINSTR;
         if (shdr->sh_type != SHT_NULL && flags != previous_flags) {
             current_offset = (current_offset + 4096 - 1) & ~(4096 - 1);
+            previous_flags = flags;
+        }
+
+        Elf64_Off predicted_offset = shdr->sh_type == SHT_NULL ? 0 : current_offset;
+
+        // Return predicted offset for the target segment
+        if (segnum == target_segnum) {
+            return predicted_offset;
+        }
+
+        // Update current offset if sh_offset is set to a custom value
+        if (shdr->sh_offset != (Elf64_Off)(-1) && shdr->sh_offset != predicted_offset) {
+            current_offset = shdr->sh_offset;
+        }
+
+        // Increase current offset by section size if not NOBITS
+        if (shdr->sh_type != SHT_NOBITS) {
+            current_offset += shdr->sh_size;
+        }
+    }
+
+    // Default to 0 if target segment not found (shouldn't happen)
+    return 0;
+}
+
+// Pure function to calculate the default address of a section
+Elf64_Addr get_default_section_address(const ElfBinary *binary, int target_segnum) {
+    Elf64_Addr current_addr = binary->ehdr.e_ehsize + binary->ehdr.e_phnum * binary->ehdr.e_phentsize;
+    int previous_flags = 0;
+
+    for (int segnum = 0; segnum <= target_segnum && segnum < binary->ehdr.e_shnum; segnum++) {
+        Elf64_Shdr *shdr = &binary->shdrs[segnum];
+
+        // Align current offset and address
+        if (shdr->sh_addralign > 0) {
+            Elf64_Off align = shdr->sh_addralign;
+            current_addr = (current_addr + align - 1) & ~(align - 1);
+        }
+
+        // Page alignment if segment flags changed
+        int flags = shdr->sh_flags & SHF_EXECINSTR;
+        if (shdr->sh_type != SHT_NULL && flags != previous_flags) {
             current_addr = (current_addr + 4096 - 1) & ~(4096 - 1);
             previous_flags = flags;
         }
-        Elf64_Off predicted_offset = shdr->sh_type == SHT_NULL ? 0 : current_offset;
+
         Elf64_Addr predicted_addr = is_file_only_section(binary, shdr) ? 0 : current_addr;
-        if (shdr->sh_offset == (Elf64_Off)(-1) || shdr->sh_offset == predicted_offset) {
-            shdr->sh_offset = predicted_offset;
-        } else {
-            current_offset = shdr->sh_offset;
+
+        // Return predicted address for the target segment
+        if (segnum == target_segnum) {
+            return predicted_addr;
         }
-        if (shdr->sh_addr == (Elf64_Off)(-1) || shdr->sh_addr == predicted_addr) {
-            shdr->sh_addr = predicted_addr;
-        } else {
+
+        // Update current address if sh_addr is set to a custom value
+        if (shdr->sh_addr != (Elf64_Off)(-1) && shdr->sh_addr != predicted_addr) {
             current_addr = shdr->sh_addr;
         }
-        if (shdr->sh_type != SHT_NOBITS) {
-            current_offset += shdr->sh_size;
-        }
+
+        // Increase current address by section size
         current_addr += shdr->sh_size;
+    }
+
+    // Default to 0 if target segment not found (shouldn't happen)
+    return 0;
+}
+
+// Updated calculate_section_offsets using the pure functions
+static void calculate_section_offsets(const ElfBinary *binary) {
+    for (int segnum = 0; segnum < binary->ehdr.e_shnum; segnum++) {
+        Elf64_Shdr *shdr = &binary->shdrs[segnum];
+
+        Elf64_Off default_offset = get_default_section_offset(binary, segnum);
+        Elf64_Addr default_addr = get_default_section_address(binary, segnum);
+
+        // Set default offset if not explicitly specified
+        if (shdr->sh_offset == (Elf64_Off)(-1) || shdr->sh_offset == default_offset) {
+            shdr->sh_offset = default_offset;
+        }
+
+        // Set default address if not explicitly specified
+        if (shdr->sh_addr == (Elf64_Off)(-1) || shdr->sh_addr == default_addr) {
+            shdr->sh_addr = default_addr;
+        }
     }
 }
 
+// Updated is_default_section_offset using the pure function
 bool is_default_section_offset(const ElfBinary *binary, int target_segnum, Elf64_Off offset) {
-    Elf64_Off current_offset = binary->ehdr.e_ehsize + binary->ehdr.e_phnum * binary->ehdr.e_phentsize;
-
-    int previous_flags = 0;
-    for (int segnum = 0; segnum < binary->ehdr.e_shnum; segnum++) {
-        Elf64_Shdr *shdr = &binary->shdrs[segnum];
-        if (shdr->sh_addralign > 0) {
-            Elf64_Off align = shdr->sh_addralign;
-            current_offset = (current_offset + align - 1) & ~(align - 1);
-        }
-        // Align to 4096 page if the segment changed (detected by flag change)
-        int flags = shdr->sh_flags & SHF_EXECINSTR;
-        if (shdr->sh_type != SHT_NULL && flags != previous_flags) {
-            current_offset = (current_offset + 4096 - 1) & ~(4096 - 1);
-            previous_flags = flags;
-        }
-        Elf64_Off predicted_offset = shdr->sh_type == SHT_NULL ? 0 : current_offset;
-        if (segnum == target_segnum) {
-            return predicted_offset == offset;
-        }
-        if (shdr->sh_type != SHT_NULL && shdr->sh_offset != (Elf64_Off)(-1)) {
-            current_offset = shdr->sh_offset;
-        }
-        if (shdr->sh_type != SHT_NOBITS) {
-            current_offset += shdr->sh_size;
-        }
-    }
-    return false;
+    Elf64_Off default_offset = get_default_section_offset(binary, target_segnum);
+    return offset == default_offset;
 }
 
 void compute_defaults(ElfBinary *binary) {
